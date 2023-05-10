c1cfbdaeed386d9b3df845189e79ceaa016e2d09
nomoresomethingwentwrong <lll109016664@outlook.com>
2023-05-10 12:33:50 +0800

    update params

--- model_utils.py
+++ model_utils.py
@@ -736,7 +736,7 @@ def get_params(dataset):
             "use_bias": False,
         },
         "gene_exp_condition_mlp": {
-            "input_feature_dim": 512 + 978 + 1,#512 + 978 + 1,  # should be 832 + 978 + 1 for AAE; +1 is for the dosage
+            "input_feature_dim": 832 + 978 + 1,#512 + 978 + 1,  # should be 832 + 978 + 1 for AAE; +1 is for the dosage
             "output_size": 512,
             "hidden_layer_dims": [],
             "use_bias": False,
